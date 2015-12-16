afb7a440ebff11f42f585d83c3c12034b1371a0f
NMS73 <semtchenok@gmail.com>
2015-12-16 05:00:15 +0300

    fffffff

--- main.cpp
+++ main.cpp
@@ -6,20 +6,22 @@ int test(const int *, int);
 
 int main()
 {
-    int a = 4;
+
+
+    int b;
     const int N = 5;
     int arr1[N] = {0,1,2,3,4};
     cout << "before: " << arr1[3] << endl;
     test(arr1,N);
     cout << "after: " << arr1[3] << endl;
     cout << "Hello World!" << endl;
-    arr1[2]=5;
-    cout << "a = " << a << "a << 1 = " << a >> 1 << endl;
+    arr1[2]=51;
+
     return 0;
 }
 
 int test(const int * , int)
 {
-    cout << "hello\n";
+    cout << "hello1\n";
     return 0;
 }
